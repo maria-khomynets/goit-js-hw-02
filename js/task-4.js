b4f8d188f4c944a5837ef663343173852280196a
Maria Khomynets <masha.khomynets@gmail.com>
2026-02-24 10:24:12 +0200

    task

--- js/task-4.js
+++ js/task-4.js
@@ -12,6 +12,7 @@ function getShippingCost(country) {
       return `Sorry, there is no delivery to your country`;
   }
 }
+
 console.log(getShippingCost('Australia')); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingCost('Germany')); // "Sorry, there is no delivery to your country"
 console.log(getShippingCost('China')); // "Shipping to China will cost 100 credits"
